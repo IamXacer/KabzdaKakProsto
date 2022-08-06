14354f1a04bd88410cdbcdf68bb28356960b467a
IamXacer <phanton7487@gmail.com>
2022-08-07 02:40:41 +0300

    Acordion

---
+++ src/components/Acordion.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Accordion() {
+    return (
+        <div>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    )
+}
+
+
+function AccordionTitle() {
+    return (
+        <div><h3>Menu</h3></div>)}
+
+
+function AccordionBody() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>)}
+
+export default Accordion
