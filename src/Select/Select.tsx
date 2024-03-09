fc746ef841b457e09caf0a6d98c0f52f705119c1
Phantom <phanton7487@gmail.com>
2024-03-09 23:17:12 +0200

    Acordion RatinG OnOff

--- src/Select/Select.tsx
+++ src/Select/Select.tsx
@@ -7,34 +7,40 @@ export type ItemType = {
 }
 
 export type SelectPropsType = {
-    value: number
+    value?: number
     onChange: (value: any) => void
     items: ItemType[]
 }
 
 export const Select = (props: SelectPropsType) => {
-    let [active, setActive] = useState(false)
-  const changeSelect = props.items.find(i=>i.value === props.value)
-
-    const TogleItems = () => setActive(!active)
-    const onItemClick = (value:any) => {
-        props.onChange(value)
-        TogleItems()
+    const [edit, setEdit] = useState(false)
+    const [hoverElement, setHoverElement] = useState(props.value)
+    const selectedItem = props.items.find(i => i.value === props.value)
+    const hoverItem = props.items.find(i => i.value === hoverElement)
+    const setEditHandler = () => {
+        setEdit(!edit)
     }
-
+    const onItemClick = (value: any) => {
+        props.onChange(value)
+        setEditHandler()
+    }
     return (
-        <> <div className={styles.select}>
+        <><div className={styles.select} onClick={setEditHandler}>
 
-                <span className={styles.main} onClick={TogleItems}>
-                   {changeSelect && changeSelect.title} //если есть селект айтем отобрази
-                </span>
-
-                {active &&
-                    <div className={styles.select}>
-                        {props.items.map(i => <div key={i.value}
-                                                   onClick={()=>onItemClick(i.value)}></div>)}
-                    </div>}
-            </div> </>
+                    <span className={styles.main}>
+                        {selectedItem && selectedItem.title}
+                    </span>
+            {!edit && <div className={styles.items}>
+                {props.items.map(i =>
+                    <div className={`${styles.item} ${hoverItem === i ? styles.selected : ''}`} key={i.value}
+                         onMouseEnter={() => {
+                             setHoverElement(i.value)}}
+                         onClick={() => onItemClick(i.value)}
+                    >{i.title}</div>)}
+            </div>
+            }
+        </div>
+        </>
     )
 }
 
