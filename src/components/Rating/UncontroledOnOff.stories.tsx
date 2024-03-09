6270696d2eb95defb74e88566ea8b92297be87a1
Phantom <phanton7487@gmail.com>
2024-03-09 23:17:35 +0200

    Acordion RatinG OnOff

--- src/components/Rating/UncontroledOnOff.stories.tsx
+++ src/components/Rating/UncontroledOnOff.stories.tsx
@@ -14,11 +14,6 @@ export const OnMode = () => <UncontroledOnOff defaultValue={true} onChange={call
 export const OffMode = () => <UncontroledOnOff defaultValue={false} onChange={callback}/>
 export const DefaultInputValue = () => <input defaultValue={'yo'} />
 
-export const ModdeChanging = () => {
-  const [value,setValue] = useState<boolean>(true)
-    return <OnOff on={value} onChange={setValue}/>
-}
-
 
 
 
--- src/components/Rating/UncontroledOnOff.tsx
+++ src/components/Rating/UncontroledOnOff.tsx
@@ -1,48 +1,51 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 type PropsType = {
-  /*  on?:boolean*/
-    onChange:( on:boolean)=>void
-    defaultValue?:boolean
+    on?: boolean
+    onChange: (on: boolean) => void
+    defaultValue?: boolean
 }
 
-export const UncontroledOnOff = (props:PropsType) => {
-let [on,setOn] =useState(props.defaultValue ? props.defaultValue:false)
-    const onStyle =  {
+export const UncontroledOnOff = (props: PropsType) => {
+    let [on, setOn] = useState(props.defaultValue ? props.defaultValue : false)
+
+    const onStyle = {
         width: '20px',
-            height: '20px',
+        height: '20px',
         border: '1px solid black',
-        background :on ?'white' :'green'
+        background: on ? 'green' : 'white' // используем локальное состояние `on`
     }
+
     const offStyle = {
         width: '20px',
         height: '20px',
         border: '1px solid black',
-        background : on ?'red':'white'
+        background: on ? 'white' : 'red' // используем локальное состояние `on`
     }
+
     const indicatorStyle = {
         width: '10px',
         height: '10px',
         borderRadius: '4px',
         border: '1px solid black',
-        background :on ?"red":'green'
-
+        background: on ? "green" : 'red' // используем локальное состояние `on`
     }
-const onClicked = () => {
-    setOn(false)
-    props.onChange(false)
-}
-const OffClicked = () => {
-    setOn(true)
-    props.onChange(true)
-}
+
+    const onClicked = () => {
+        setOn(false); // изменяем локальное состояние
+        props.onChange(true);
+    }
+
+    const offClicked = () => {
+        setOn(true); // изменяем локальное состояние
+        props.onChange(true);
+    }
 
     return (
         <div>
-            <div style={onStyle} onClick={onClicked} >On</div>
-            <div style={offStyle} onClick={OffClicked}>Off</div>
+            <div style={onStyle} onClick={onClicked}>On</div>
+            <div style={offStyle} onClick={offClicked}>Off</div>
             <div style={indicatorStyle}></div>
         </div>
     );
 };
-
